0cab4e2f2378e15d5ff06b50d788de71e17eb444
ferlicio <ferlicio@gmail.com>
2022-07-15 23:21:04 -0300

    atualização 15/07/2022

---
+++ soulcode-bc20-rxjs-class/src/index.ts
@@ -0,0 +1,164 @@
+import fs, { read } from 'fs' // manipula os arquivos do sistema
+import path from 'path' // para gerar o caminho para os arquivos
+import chalk from 'chalk' // para colorir o console :)
+import { Observable, Subject, Subscriber, filter, map, take, first, last, catchError, of, throwError, retry, timeout } from 'rxjs'
+import { Console } from 'console'
+
+const isCSS = /.+\s*{\s*(.+:\s*.+;\s*)*}\s*/gmi // expressão regular para pegar arquivos CSS
+const isHTML = /^<!DOCTYPE html>/ig // expressão regular para pegar arquivos HTML
+
+const files: string[] = [
+  path.join(__dirname, 'texto1.txt'),
+  path.join(__dirname, 'html1.html'),
+  path.join(__dirname, 'css1.css'),
+  path.join(__dirname, 'texto2.txt'),
+  path.join(__dirname, 'html2.html'),
+  path.join(__dirname, 'css2.css'),
+  path.join(__dirname, 'texto3.txt'),
+  path.join(__dirname, 'html3.html'),
+  path.join(__dirname, 'css3.css'),
+  path.join(__dirname, 'texto4.txt'),
+  path.join(__dirname, 'html4.html'),
+  path.join(__dirname, 'css4.css'),
+  path.join(__dirname, 'css4.css'),
+  path.join(__dirname, 'js1.js'),
+]
+
+// receber o caminho dos arquivos que precisam ser lidos e retornados
+function readFiles(arquivos: string[], hot: boolean) {
+  const leitorDeArquivos$: Observable<string> = new Observable((subscriber: Subscriber<string>) => {
+    // 1° Estágio: Sucesso (next) -> Ele conseguiu enviar os dados com sucesso
+    // 2° Estágio: Erro (error) -> Algum problema ocorreu na execução do Observable
+    // 3° Estágio: Concluído (complete) -> Termina de enviar todos os dados do Observable
+
+    // 1° - Percorrer o array de arquivos e fazer a leitura deles
+    let i = 0
+
+    const idInterval = setInterval(() => {
+      if (i > arquivos.length - 1) {
+        /**
+         * quando a função complete for chamada, significa que o observables já te
+         * enviou todos os dados com sucesso e não há mais nada para enviar
+         */
+        subscriber.complete()
+
+        // clearInterval irá parar a execução do setInterval, evitando
+        // que ela seja infinito
+        clearInterval(idInterval)
+      } else {
+        try {
+          /**
+           * a função readFileSync do fs faz a leitura de um arquivo
+           * de maneira síncrona e retorna o conteúdo do arquivo como uma string
+           */
+          const arquivo = arquivos[i]
+          const conteudoArquivo = fs.readFileSync(arquivo, { encoding: 'utf-8' })
+  
+          // a função é responsável por informar que ocorreu sucesso no observable
+          // enviando para os observadores
+          subscriber.next(conteudoArquivo)
+        } catch (error) {
+          /**
+           * O error é chamado quando acontece algum problema na fonte de dados
+           * Quando ele é chamado, a execução do Observable para automaticamente
+           */
+          subscriber.error(error)
+        }
+      }
+
+      i++
+    }, 2500)
+  })
+
+  if (hot == true) {
+    const subLeitor$: Subject<string> = new Subject()
+    leitorDeArquivos$.subscribe(subLeitor$)
+
+    return subLeitor$
+  }
+
+  return leitorDeArquivos$
+}
+
+const leitor$ = readFiles(files, true)
+
+/**
+ * No momento que a função subscribe é executada, eu estou
+ * falando para a fonte de dados que há um novo Observador
+ * daqueles dados, ou seja, mais um lugar que a stream de dados
+ * deve enviar esses dados
+ */
+
+/**
+ * 1° -> Sucesso (next)
+ * 2° -> Erro (error)
+ * 3° -> Concluído (complete)
+ */
+leitor$
+.pipe(
+/*   filter((conteudo) => {
+    return isHTML.test(conteudo)
+  }),
+  map((html) => {
+    return html.length
+  }) */
+
+  /* map((conteudo) => {
+    return conteudo.length
+  }),
+  map((qtdCaracteres) => {
+    return qtdCaracteres * 3
+  }) */
+
+  //take(5)
+  //first()
+  /* first((conteudo) => {
+    return !isCSS.test(conteudo) && !isHTML.test(conteudo)
+  }) */
+  //last()
+  /* last((conteudo) => {
+    return isHTML.test(conteudo)
+  }) */
+  /* catchError((erro) => {
+    console.log(chalk.italic(chalk.red('OCORREU UM ERRO NO OBSERVABLE')))
+    //return of([erro])
+    return throwError(erro)
+  }) */
+  //retry(3)
+  timeout(3000)
+)
+.subscribe(
+  // next
+  (conteudoArquivo) => {
+    console.log(chalk.green('SUB 1 - Texto lido com sucesso!\n'))
+    console.log(conteudoArquivo + '\n')
+  },
+  // error
+  (erro) => {
+    console.log(chalk.red('Ocorreu um erro na execução\n'))
+    console.log(erro)
+  },
+  //complete
+  () => {
+    console.log(chalk.blue('Todos os arquivos foram lidos :)'))
+  }
+)
+
+setTimeout(() => {
+  leitor$.subscribe(
+    // next
+    (conteudoArquivo) => {
+      console.log(chalk.green('SUB 2 - Texto lido com sucesso!\n'))
+      console.log(conteudoArquivo + '\n')
+    },
+    // error
+    (erro) => {
+      console.log(chalk.red('Ocorreu um erro na execução\n'))
+      console.log(erro)
+    },
+    //complete
+    () => {
+      console.log(chalk.blue('Todos os arquivos foram lidos :)'))
+    }
+  )
+}, 10000)
