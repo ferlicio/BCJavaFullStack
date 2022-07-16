ef1f9133c9557494ad7597b070274c66a086a556
ferlicio <ferlicio@gmail.com>
2022-07-16 00:38:56 -0300

    atualização 16/07/2022

--- GitPushMain.bat
+++ GitPushMain.bat
@@ -6,6 +6,8 @@ cls
 REM echo coloque a senha para atualizar o repositório do Git
 REM set /p senha=
 REM 	if %senha% NEQ 91378250 GOTO senhaerrada
+REM ================================================================
+
 git add .
 git commit -m "atualização %date%"
 git push origin main
