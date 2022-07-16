e8533a7757f6898dac59727797c897656472f0de
ferlicio <ferlicio@gmail.com>
2022-07-15 23:26:27 -0300

    atualização 15/07/2022

--- Atualizar Git.bat
+++ Atualizar Git.bat
@@ -3,6 +3,9 @@ echo OFF
 cls
 
 
+echo coloque a senha para atualizar o repositório do Git
+set /p senha=
+	if %senha% NEQ 91378250 exit
 git add .
 git commit -m "atualização %date%"
 git push origin main
