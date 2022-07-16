c3f65659c64acc1db4f4d9796dfb0f84ff328008
ferlicio <ferlicio@gmail.com>
2022-07-15 23:18:49 -0300

    atualização 15/07/2022

--- Atualizar Git.bat
+++ Atualizar Git.bat
@@ -2,4 +2,7 @@ chcp 65001
 echo OFF
 cls
 
-start "" "%PROGRAMFILES%\Git\bin\sh.exe" -c "git add . && git commit -m "atualização" && git push origin main; bash"
+
+git add .
+git commit -m "atualização %date%"
+git push origin main
