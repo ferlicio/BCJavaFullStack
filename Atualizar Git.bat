b93393f2eca08c9ce3ec943361de06aa8dbcc956
ferlicio <ferlicio@gmail.com>
2022-07-15 23:28:43 -0300

    atualização 15/07/2022

--- Atualizar Git.bat
+++ Atualizar Git.bat
@@ -5,7 +5,13 @@ cls
 
 echo coloque a senha para atualizar o repositório do Git
 set /p senha=
-	if %senha% NEQ 91378250 exit
+	if %senha% NEQ 91378250 GOTO senhaerrada
 git add .
 git commit -m "atualização %date%"
 git push origin main
+
+
+:senhaerrada
+cls
+echo senha errada...
+@pause
