26b3cfb275ebe631c43f23ac34d2a5bcbe414705
ferlicio <ferlicio@gmail.com>
2022-07-18 14:43:44 -0300

    atualização 18/07/2022

--- Angular/angular-todo-app/src/app/app.module.ts
+++ Angular/angular-todo-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import {environment} from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import {environment} from '../environments/environment';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
