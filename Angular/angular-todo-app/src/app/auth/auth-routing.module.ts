18e51cd8a9513cdfff087757b6c46f047ce2f85c
ferlicio <ferlicio@gmail.com>
2022-07-19 09:58:47 -0300

    atualização 19/07/2022

--- Angular/angular-todo-app/src/app/auth/auth-routing.module.ts
+++ Angular/angular-todo-app/src/app/auth/auth-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmailVerificationComponent } from './pages/email-verification/email-verification.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'verify-email',
+    component: EmailVerificationComponent
   }
 ];
 
